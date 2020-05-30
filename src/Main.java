c2d31cc477935f3d2b74181570cf8a153c7cc251
Jana C <janakatharina.cording@posteo.de>
2020-05-30 22:15:03 +0200

    modify user prompts

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,15 @@ public class Main {
 		System.out.println("This app will take different amounts of 'coins', holding diverse values.");
 		System.out.println("(Say, the contents of your wallet: multiple 50 ct, 20 ct, 1 € etc. pieces.)");
 		System.out.println("It then generates n subsets of this set of coins, all holding the exact same sum value.");
-		System.out.println("(Let's say, in case you want to equally distribute these coins among your children.)");
+		System.out.println("(Let's say, in case you want to distribute this money among your children, equally.)");
 		System.out.println("It will let you know if this is possible, and if yes, show you what these subsets look like.\n");
 		
-		System.out.println("To give it a try, you may want to use these values:\n");
+		System.out.println("To give it a try, you may want to use these parameters:\n");
+		
+		System.out.println("Value 1 : 5 coins.");
+		System.out.println("Value 3 : 3 coins.");
+		System.out.println("Value 5 : 2 coins.\n");
+		System.out.println("n : 2");
 		
 		Talerverteilung tal = new Talerverteilung();
 		tal.execute();
